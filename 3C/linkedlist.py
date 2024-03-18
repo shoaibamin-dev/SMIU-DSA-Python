2ec73d55bcf0f794f7b90ccbeb2453cc4721d8fe
Shoaib Amin <shoaibsivany@hotmail.com>
2024-03-18 11:52:14 +0500

    linkedlist

--- 3C/linkedlist.py
+++ 3C/linkedlist.py
@@ -1,18 +1,33 @@
 class Node:
-    def __init__(self, val, next):
+    def __init__(self, val, next=None):
         self.val = val
         self.next = next
     
+    
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def add(self, val):
+        if self.head == None:
+            self.head = Node(val, None)
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = Node(val, None)
+    
     def show(self):
-        cur = self
+        cur = self.head
         while cur != None:
             print(cur.val)
             cur = cur.next
 
-n5 = Node("there", None)
-n4 = Node("you", n5)
-n3 = Node("are", n4)
-n2 = Node("how", n3)
-n1 = Node("hello", n2)
 
-n1.show()
+ll = Linkedlist()
+ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(4)
+
+ll.show()
